49765cb4416adf9e55535af75bf09138a0605d9a
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-21 21:45:31 -0600

    updated js

---
+++ week1/p5intro/p5test.js
@@ -0,0 +1,19 @@
+const color = window.prompt("What is your favorite color?");
+console.log(color);
+
+function setup() {
+  createCanvas(300, 500);
+}
+
+function draw() {
+  background(220);
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill (255);
+    }
+        ellipse(mouseX,mouseY,80,80)
+    }  
