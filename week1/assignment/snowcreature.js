08d0e29a8185051fe3bb8aeb49451fdc75a20b7b
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-24 15:26:44 -0600

    Update snowcreature.js

--- week1/assignment/snowcreature.js
+++ week1/assignment/snowcreature.js
@@ -4,7 +4,16 @@ function setup() {
     background("#ccc");
     grid = loadImage("100px_grid.png");
 }
-
 function draw() {
-    background(grid)
+    background(220);
+    // snowman legs
+    fill("#f1f1f1");
+    stroke(100, 250, 100);
+    strokeWeight(20);
+    // left
+    ellipse(350, 650, 200);
+    // right
+    ellipse(650, 650, 200);
+    //body
+    ellipse(500, 450, 350, 400);
 }
