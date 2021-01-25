a6348e94ab52f50e6fe1651d2f00863476a3ff5e
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-24 20:42:22 -0600

    Update snowcreature.js

--- week1/assignment/snowcreature.js
+++ week1/assignment/snowcreature.js
@@ -1,22 +1,27 @@
 let grid = undefined;
-let stroke1 = prompt ("enter a basic color name in lowercase", "gray");
-let stroke2 = prompt ("enter another basic color name in lowercase", "purple");
+let fill1 = prompt ("enter a basic color name in lowercase", "lightblue");
+let fill2 = prompt ("enter another basic color name in lowercase", "brown");
 function setup() {
-    createCanvas(1000, 1000);
-    background("#ccc");
-    grid = loadImage("100px_grid.png");
+  createCanvas(1000, 1000);
 }
+
+
 function draw() {
-    background(220);
-    // bernie the snowman
-    // head and hair
+    // background
+
+    // BERNIE
+    // bernie head
+    fill(255, 255, 255);
+    stroke(0);
+    strokeWeight(1)
     arc(500, 200, 200, 200, radians(180), 0)
+    fill(248, 233, 211);
     ellipse(500, 200, 175, 200);
     // ears
     ellipse(405, 200, 20, 40);
     ellipse(595, 200, 20, 40);
-    // mask
-    quad(425, 215, 575, 215, 560, 290, 440, 290);
+    // mask fill(fill1);
+    quad(425, 215, 575, 215, 560, 290, 440, 290);   
     arc(500, 235, 90, 5, radians(180), 0)
     arc(500, 260, 90, 5, 0, radians(180))
     line(415, 190, 425, 215);
@@ -27,21 +32,30 @@ function draw() {
     line(440, 180, 490, 190);
     line(510, 190, 560, 180);
     // body
+    fill(92, 92, 92)
     quad(400, 300, 600, 300, 650, 500, 350, 500);
     rect(400, 500, 200, 60);
     // legs
-    ellipse(470, 675, 75, 200);
+    fill(26, 44, 44);
+    strokeWeight(10);
+    ellipse(500, 575, 200, 100);
+    ellipse(470, 700, 75, 250);
     quad(390, 550, 550, 515, 575, 570, 400, 615);
-    ellipse(540, 615, 75, 200);
+    ellipse(540, 650, 75, 250);
     // feet
-    quad(450, 755, 520, 755, 530, 800, 450, 800);
-    quad(520, 700, 600, 680, 600, 725, 525, 740);
+    fill(95, 58, 43);
+    quad(445, 780, 520, 780, 530, 825, 445, 825);
+    quad(510, 740, 600, 700, 600, 750, 520, 775);
     // arms
+    strokeWeight(1);
     line(490, 400, 400, 415);
     line(435, 410, 430, 350);
     line(565, 410, 570, 350);
     line(440, 200, 490, 200);
     // mitten1
+    fill(fill2)
+    stroke(95, 58, 43)
+    strokeWeight(5)
     beginShape()
     vertex(490, 400)
     vertex(520, 400)
@@ -63,37 +77,5 @@ function draw() {
     vertex(510, 500)
     vertex(520, 475)
     vertex(510, 450)
-    endShape()  
-
-    
-
-
-    // snowman
-    // snowman legs
-    fill("#f1f1f1");
-    stroke(stroke1);
-    strokeWeight(20);
-    // left
-    ellipse(350, 650, 200);
-    // right
-    ellipse(650, 650, 200);
-    // body
-    ellipse(500, 450, 350, 400);
-    // head
-    ellipse(500, 200, 200);
-    // hat brim
-    stroke(stroke2);
-    strokeWeight(40);
-    line(400, 120, 600, 120);
-    // hat body
-    quad(400, 50, 600, 50, 550, 120, 450, 120);
-    // eyes
-    stroke(0);
-    strokeWeight(50);
-    point(425, 200);
-    point(575, 200);
-    // mouth
-    nofill()
-    strokeWeight(10)
-    arc(500, 200, 80, 80, 0, HALF_PI)
-}
+    endShape()
+}   
