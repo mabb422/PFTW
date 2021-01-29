20b469af1bd89fbacea5251d4f3b1c4f1114d077
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-28 20:08:54 -0600

    Arrays

---
+++ week2/arrays/arrays.js
@@ -0,0 +1,9 @@
+const movies = window.prompt("What is your favorite movie?");
+let myArr = [
+    "Blazing Saddles", 
+    "The Producers", 
+    "Young Frankenstein", 
+    "Men in Tights", 
+    "Spaceballs"
+];
+console.log(myArr);
