47ae2a75aa0a21cfeb8d14fd037ed060b02a0036
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-28 21:58:34 -0600

    Css, Remaining JS

--- week2/objects/objects.js
+++ week2/objects/objects.js
@@ -2,39 +2,42 @@
 const games = window.prompt("Of my 5 favorite games, which number do you want to know about?")
 const myGames = [{ 
     game: "Cribbage", 
-    gameType: "Card Game", 
+    gameType: "card game", 
     maxPlayers: 3, 
-    bestToken: "The Darkest Pegs", 
+    bestToken: "the darkest pegs available", 
     shortDescription: "Do Math. Move Pegs. Ruthelessly steal points if your house rules allow it"
     },
     {
     game: "Trivial Persuit", 
-    gameType: "Board Game", 
+    gameType: "board game", 
     maxPlayers: 6, 
     bestToken: "purple", 
     shortDescription: "Finally use all your useless knowledge, get correct answers in 6 categories to win."
     },
     {
     game: "Monopoly", 
-    gameType: "Board Game", 
+    gameType: "board game", 
     maxPlayers: 8, 
     bestToken: "Scottie Dog", 
     shortDescription: "Buy Properties. Build a Hotel Empire. Collect $200. Ruin Friendships"
     },
     {
     game: "Gin Rummy", 
-    gameType: "Card Game", 
+    gameType: "card game", 
     maxPlayers: 3, 
-    bestToken: "Not Applicable", 
+    bestToken: "not applicable", 
     shortDescription: "Make sets of 3-4 cards in a single number or runs in one suit. Yell 'Thats the one!' when you win."
     },
     {
     game: "Jigsaws", 
-    gameType: "Puzzle Game", 
-    maxPlayers: "Infinite", 
-    bestToken: "Not Applicable", 
+    gameType: "puzzle game", 
+    maxPlayers: "infinite", 
+    bestToken: "not applicable", 
     shortDescription: "Waste hours putting pieces together just for the thrill of completion. You win if the dog doesn't eat any pieces." 
-}]
+}];
 window.alert(
-    "You've selected #" +
-)
+    "You've selected " + myGames[games - 1].game +
+    ", a " + myGames[games - 1].gameType +
+    " with an optimal maximum of " + myGames[games - 1].maxPlayers +
+    " players. The best game token is " + myGames[games - 1].bestToken +
+    ". How do you play, you ask? " + myGames[games - 1].shortDescription);
