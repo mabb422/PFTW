0c5cacf1a2d66b56aed239f4b7387b518b3c01fc
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-28 21:44:10 -0600

    objects set up

---
+++ week2/objects/objects.js
@@ -0,0 +1,40 @@
+
+const games = window.prompt("Of my 5 favorite games, which number do you want to know about?")
+const myGames = [{ 
+    game: "Cribbage", 
+    gameType: "Card Game", 
+    maxPlayers: 3, 
+    bestToken: "The Darkest Pegs", 
+    shortDescription: "Do Math. Move Pegs. Ruthelessly steal points if your house rules allow it"
+    },
+    {
+    game: "Trivial Persuit", 
+    gameType: "Board Game", 
+    maxPlayers: 6, 
+    bestToken: "purple", 
+    shortDescription: "Finally use all your useless knowledge, get correct answers in 6 categories to win."
+    },
+    {
+    game: "Monopoly", 
+    gameType: "Board Game", 
+    maxPlayers: 8, 
+    bestToken: "Scottie Dog", 
+    shortDescription: "Buy Properties. Build a Hotel Empire. Collect $200. Ruin Friendships"
+    },
+    {
+    game: "Gin Rummy", 
+    gameType: "Card Game", 
+    maxPlayers: 3, 
+    bestToken: "Not Applicable", 
+    shortDescription: "Make sets of 3-4 cards in a single number or runs in one suit. Yell 'Thats the one!' when you win."
+    },
+    {
+    game: "Jigsaws", 
+    gameType: "Puzzle Game", 
+    maxPlayers: "Infinite", 
+    bestToken: "Not Applicable", 
+    shortDescription: "Waste hours putting pieces together just for the thrill of completion. You win if the dog doesn't eat any pieces." 
+}]
+window.alert(
+    "You've selected #" +
+)
