9412fc37f75d53451aa6dd28b34fe71be871c62b
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-01-31 13:07:18 -0600

    Update quiz.js

---
+++ week2/assignment/quiz.js
@@ -0,0 +1,9 @@
+const question = [
+    {question: 'What runs around the whole yard without moving?', answer: 'a fence'}
+    {question: 'What can you catch but never throw?', answer: 'a cold'}
+    {question: 'What travels around the world without ever leaving the corner?', answer: 'a stamp'}
+    {question: 'What has many eyes, yet cannot see?', answer: 'potatoes'}
+    {question: 'What goes up and down the stairs without ever moving?', answer: 'carpet'}
+    {question: 'What has no hinges, lock, or lid, but inside a golden treasure is hid?', answer: 'an egg'}
+    {question: 'What follows you wherever you go, but the more of me you take, the more you leave behind?', answer: 'footsteps'}
+]
