1211c406361e2b51576bbf2377b5d0e8019cf39a
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-08 00:14:39 -0600

    Update spirograph.js

--- week3/assignment2/spirograph.js
+++ week3/assignment2/spirograph.js
@@ -1,34 +1,37 @@
-let rotateBy = 5;
+let vert1 = prompt ("Pick a number 20-50", "20");
+let vert2 = prompt ("Pick a number 5-50", "25");
+let border = prompt ("What is your favorite basic color in lowercase?", "white");
+let rotateBy = 10;
 
 function setup() {
     createCanvas(1000, 1000);
-    background(0);
+    background('purple');
     angleMode(DEGREES);
 }
-
+//object shapes
 function makeArm(rotateBy) {
     let alt = Math.round(rotateBy / 360);
     fill(100, 255, 255, 25);
-    stroke(255);
+    stroke(border);
     strokeWeight(2);
     beginShape();
-    vertex(25, 25 / alt);
+    vertex(vert2, 25 / alt);
     vertex(100, -80);
     vertex(-45, 80 * alt);
     vertex(30, 45 * alt)
     vertex(25, 25 / alt);
     endShape();
     fill(255, 0, 100, 50);
-    triangle(300 + alt, 300, 100 + alt, 50, 50 + alt, 50)
-
+    triangle(vert1 + alt, 300, 100 + alt, 50, 50 + alt, 50)
 }
-
+//rotation
 function draw() {
     translate(500, 500);
     rotate(rotateBy);
     makeArm(rotateBy);
     rotateBy += 15;
 }
+//Start/Stop motion
 function mousePressed() {
     noLoop();
 }
