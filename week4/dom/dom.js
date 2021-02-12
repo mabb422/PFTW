fdd6d2a7dfb9cb34bc59f75a0f37aa9282e5c037
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-11 23:18:24 -0600

    Update dom.js

--- week4/dom/dom.js
+++ week4/dom/dom.js
@@ -1,12 +1,13 @@
 // select empty div from HTML and assign to variable
-const myEmptyDiv = document.querySelector(myEmptyDiv);
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
 // put h1 inside of it
 // create new element
 const heading = document.createElement('h1');
-heading.innerHTML - 'Hello!';
+heading.innerHTML - 'Did you know llamas can serve as herd guards? That\'s llamazing!';
+heading.style.cursor = 'ponter';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
 function handleHeadingClick() {
-    console.log('Heading had been clicked');
+    console.log('Heading has been clicked');
     document.body.style.backgroundColor = 'yellow';
 }
