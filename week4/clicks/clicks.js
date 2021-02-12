8160d420bb21dfdf19169574bbc1151bba806549
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-11 23:55:49 -0600

    Update clicks.js

--- week4/clicks/clicks.js
+++ week4/clicks/clicks.js
@@ -1,5 +1,8 @@
 let ellipseX = 0;
+let ellipseY = random(height - ellipseHeight);
 let elliWidth = 50;
+let elliHeight = 50;
+let clickHeight = 0;
 function setup () {
     createCanvas(500, 500);
 }
@@ -8,10 +11,15 @@ function draw () {
     background(100, 100, 200); 
     drawShape();
     ellipseX++;
+    if(ellipseX > width) {
+        noLoop();
+        Text('Your Score was ' + clickCount, 100, 300);
+    }
 }
 
 function mousePressed () {
-    if ((mouseX >= ellipseX && mouseX <= ellipseX + elliWidth) && (mouseY >= 0 && mouseY <= 50)) {
+    if ((mouseX >= ellipseX && mouseX <= ellipseX + elliWidth) && (mouseY >= ellipseY && mouseY <= ellipseY + elliHeight)) {
+        clickCount++;
         console.log('hit');
     }
 }
