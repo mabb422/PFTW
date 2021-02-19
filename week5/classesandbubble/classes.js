de0be7265d6efc9483fcab778cd9946cbce33472
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-18 21:12:08 -0600

    Update classes.js

--- week5/classesandbubble/classes.js
+++ week5/classesandbubble/classes.js
@@ -1,9 +1,40 @@
+let bubble1;
+let bubble2;
+let bubble3;
+let bubble4;
 function setup() {
     createCanvas(400, 400);
-    background(200, 150, 100);
+    background(100, 175, 150);
+    bubble1 = new Bubble();
+    bubble2 = new Bubble();
+    bubble3 = new Bubble();
+    bubble4 = new Bubble();
 }
 function draw() {
-    ellipse(100, 100, 50, 85)
+    bubble1.move();
+    bubble1.show();
+    bubble2.move();
+    bubble2.show();
+    bubble3.move();
+    bubble3.show();
+    bubble4.move();
+    bubble4.show();
 }
-//Class Bubble
-constructer()
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 200;
+    }
+    move() {
+        this.x = this.x + random(-15, 15);
+        this.y = this.y + random(-15, 15);
+    }
+    show() {
+        stroke(65, 255, 240)
+        strokeWeight(2)
+        fill(65, 255, 240, 10)
+        ellipse(this.x, this.y, 50, 85)
+    }
+}
+
+
