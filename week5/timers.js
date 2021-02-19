00fc3de5667b862e5821d4a267d1319aa0c9f17e
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-18 19:17:14 -0600

    timers set-up

---
+++ week5/timers.js
@@ -0,0 +1,12 @@
+//setup
+function setup() {
+    createCanvas(600, 600);
+    background(100, 200, 100);
+    drawBlock();
+} 
+
+function drawBlock() {
+    stroke(147, 153, 230);
+    fill(147, 153, 230);
+    rect(100, 150, 80, 165); 
+}
