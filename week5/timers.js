eb5f52c38a61babec2357776edf19078c3c0aaaf
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-18 20:16:12 -0600

    Update timers.js

--- week5/timers.js
+++ week5/timers.js
@@ -1,10 +1,10 @@
 //setup
 let blockX = 0;
 let blockY = 0;
-let blockColor = 255;
+let blockColor = 200;
 let drawTimer;
 const speed = 50;
-const distance = 20;
+const distance = 10;
 function setup() {
     createCanvas(600, 600);
     background(200, 50, 100);
@@ -21,25 +21,30 @@ function keyTyped () {
     if (isNaN(keyToNumber)){
         return;
     }
-    keyToNumber = map(keyToNumber, 1, 9, 1, 255);
+    keyToNumber = map(keyToNumber, 1, 9, 1, 200);
     console.log('converted number', keyToNumber);
     blockColor = keyToNumber;
 }
 
 //timer setup
-drawTimer = window.setInterval(() => {
-    if(blockY - 50 <= height) {
-    drawBlock(blockX, blockY, blockColor);
-    blockY += distance;
-    } else {
-    //moves starting point on X or Y
-        blockY = 0;
-        blockX += 100;
-    }
-    //stop movement
-    if(blockY - 50 > height && blockX - 50 > width) {
-        window.clearInterval(drawTimer);
-        alert('done');
-    }
-        
-}, speed);
+window.setTimeout(() => {
+    drawTimer = window.setInterval(() => {
+        if(blockY - 50 <= height) {
+        drawBlock(blockX, blockY, blockColor);
+        blockY += distance;
+        } else {
+        //moves starting point on X or Y
+            blockY = 0;
+            blockX += 100;
+        }
+        //stop movement
+        if(blockY - 50 > height && blockX - 50 > width) {
+            
+            // cancels timer
+            window.clearInterval(drawTimer);
+            alert('done');
+        }
+            
+    }, speed);
+    
+}, 1500)
