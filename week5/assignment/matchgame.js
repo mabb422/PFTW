0822b302204078f63e7c9cffa23c068c2478d1fa
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-21 20:22:27 -0600

    set up grid

--- week5/assignment/matchgame.js
+++ week5/assignment/matchgame.js
@@ -8,37 +8,47 @@
 let myCard;
 const DOWN = 'down';
 const UP = 'up';
+let startingX = 25;
+let startingY = 150;
+let cards = [];
+const gameState = {
+
+};
 function setup() {
     createCanvas(1100, 1200);
     background(218, 241, 255);
     //start grid
-    //for (let k = 0; k < 4; k++){
-        //for (let i = 0; i < 5; i++ ) {
+    for (let k = 0; k < 4; k++) {
+        for (let i = 0; i < 5; i++ ) {
+            cards.push(new Card(startingX, startingY));
+            startingX += 211;
             //rect(rectX, rectY, rectWidth, rectHeight);
             //myRect.push({ x: rectX, y: rectY, id: startingId});
             //rectX += 211; //position of new rect's X
             //startingId++; 
-        //}
+        }
+        startingY += 261;
+        startingX = 25;
         //rectX = 25;
         //rectY += 260; //position of new rect's Y
-    //}
+    }
     //console.log(myRect); 
-    myCard = new Card();
+    //myCard = new Card();
 }
 //identify which card is clicked/console output
 function mousePressed() {
-    console.log(myCard.didHit(mouseX, mouseY));
-    
-    //for(let j = 0; j < myRect.length; j++) {
+    for(let j = 0; j < cards.length; j++) {
+        if(cards[j].didHit(mouseX, mouseY)) {
+            console.log('flipped');
     //    let distance = dist(mouseX, mouseY, myRect[j].x, myRect[j].y);
     //if (distance < rectWidth / 2) {
     //    console.log('Card has been clicked', myRect[j].id);
-    //}}
+    }}
 }
 class Card {
-    constructor() {
-        this.x = 25;
-        this.y = 150;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
         this.width = 200;
         this.height = 250;
         this.face = DOWN;
