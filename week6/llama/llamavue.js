dae93e19b96b7b3d0a322a9836b7a0311552c410
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-02-28 23:43:48 -0600

    Update llamavue.js

--- week6/llama/llamavue.js
+++ week6/llama/llamavue.js
@@ -1,60 +1,15 @@
 new Vue({
-    el: "glama",
+    el: '#llamafacts',
     data: {
-      message: "The scientific name for a llama is the 'Lama Glama'."
-    }
-});
-new Vue({
-    el: "peru",
-    data: {
-      message: "The llama is the national symbol of Peru, where wild populations are particularly prominent."
-    }
-});
-new Vue({
-    el: "history",
-    data: {
-      message: "Over 3 million years ago Llamas were native to North American plains, but gradually migrated to South American mountains."
-    }
-});
-new Vue({
-    el: "baby",
-    data: {
-      message: "A baby llama is called a 'cria'."
-    }
-});
-new Vue({
-    el: "family",
-    data: {
-      message: "Llamas are highly social animals, and care for other llamas in their family group of up to around 20 llamas."
-    }
-});
-new Vue({
-    el: "guard",
-    data: {
-      message: "Llamas make good guard animals for sheep herds because they alert and protect the herd when they spot predators."
-    }
-});
-new Vue({
-    el: "age",
-    data: {
-      message: "Llamas can live to be 20-30 years old."
-    }
-});
-new Vue({
-    el: "fur",
-    data: {
-      message: "Short coated llamas are called Ccara and medium coated llamas are called Curaca"
-    }
-});
-new Vue({
-    el: "wild",
-    data: {
-      message: "Llamas have two wild camelid cousins that have never been domesticated: the vicuña (closely related to alpacas) and the guanaco."
-    }
-});
-new Vue({
-    el: "mn",
-    data: {
-      message: "There is a llama farm in Waconia MN where you can visit llamas, do a llama summer camp, or throw a llama birthday party."
+        glama: 'The scientific name for a llama is the Lama Glama.',
+        peru: 'The llama is the national symbol of Peru, where wild populations are particularly prominent.',
+        history: 'Over 3 million years ago Llamas were native to North American plains, but gradually migrated to South American mountains.',
+        baby: 'A baby llama is called a cria.',
+        family: 'Llamas are highly social animals, and care for other llamas in their family group of up to around 20 llamas.',
+        guard: 'Llamas make good guard animals for sheep herds because they alert and protect the herd when they spot predators.',
+        age: 'Llamas can live to be 20-30 years old.',
+        fur: 'Short coated llamas are called Ccara and medium coated llamas are called Curaca',
+        wild: 'Llamas have two wild camelid cousins that have never been domesticated: the vicuña (closely related to alpacas) and the guanaco.',
+        mn: 'There is a llama farm in Waconia MN where you can visit llamas, do a llama summer camp, or throw a llama birthday party.',
     }
 });
