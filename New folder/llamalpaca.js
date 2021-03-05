417def120235cefa7758c24d1450dd82635a1f56
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-03-05 00:26:55 -0600

    CODING

---
+++ New folder/llamalpaca.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+    el: '#llamalpaca',
+    data: {
+        llama: "https://en.wikipedia.org/wiki/Llama",
+        alpaca: "https://en.wikipedia.org/wiki/Alpaca",
+        headstyle: {
+            color: "Green",
+            fontSize: "70px",
+        },
+    }
+});
