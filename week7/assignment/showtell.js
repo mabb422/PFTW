9992fe38b0992db1c76d18204a2de6416c9242bd
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-03-14 22:14:15 -0500

    updates to JS and html

--- week7/assignment/showtell.js
+++ week7/assignment/showtell.js
@@ -1,41 +1,37 @@
-var gmaArt = [
-    {
-        nickname: "The Ancient Map",
-        formerLocation: "The Spanish Room",
-        currentLocation: "Living Room",
-        description: "Golden-framed map of the world featuring eight different angles of the globe with several unexplored territories left uncompleted."
-    }
-    {
-        nickname: "The Farm House",
-        formerLocation: "The Country Room",
-        currentLocation: "Upstairs Hallway",
-        description: "Impressionist painting of a very old dilapidated barn with trees and bushes surrounding it. Recently obtained from a giftshop prior to it's gifting."
-    }
-    {
-        nickname: "Miriam Plate",
-        formerLocation: "Kitchen",
-        currentLocation: "Kitchen",
-        description: "A decorative plate featuring Miriam carrying a basket. Most notable for it's placement in my grandmas kitchen stairs for the last 20-100 years, now resides in my kitchen."
-    }
-    {
-        nickname: "Caballero Poster",
-        formerLocation: "The Spanish Room",
-        currentLocation: "Upstairs Hallway",
-        description: "A poster I bought in Sevilla, Spain and gifted to grandmas Spanish room, which was regifted back to me during downsizing."
-    }
-    {
-        nickname: "The Rainy City",
-        formerLocation: "Closet",
-        currentLocation: "Living Room",
-        description: "A very blue, somewhat abstract painting of a cityscape with a bridge and reflective water in the foreground. It was found in the back of a closet."
-    }
-
-]
-
-var ga = new Vue({
-    el: '#art',
+let vm = new Vue({
+    el: "#myCollection"
     data: {
-        art: gmaArt 
-        },
+        art: [
+            {
+                nickname: "The Ancient Map",
+                formerLocation: "The Spanish Room",
+                currentLocation: "Living Room",
+                description: "Golden-framed map of the world featuring eight different angles of the globe with several unexplored territories left uncompleted."
+            },
+            {
+                nickname: "The Farm House",
+                formerLocation: "The Country Room",
+                currentLocation: "Upstairs Hallway",
+                description: "Impressionist painting of a very old dilapidated barn with trees and bushes surrounding it. Recently obtained from a giftshop prior to it's gifting."
+            },
+            {
+                nickname: "Miriam Plate",
+                formerLocation: "Kitchen",
+                currentLocation: "Kitchen",
+                description: "A decorative plate featuring Miriam carrying a basket. Most notable for it's placement in my grandmas kitchen stairs for the last 20-100 years, now resides in my kitchen."
+            },
+            {
+                nickname: "Caballero Poster",
+                formerLocation: "The Spanish Room",
+                currentLocation: "Upstairs Hallway",
+                description: "A poster I bought in Sevilla, Spain and gifted to grandmas Spanish room, which was regifted back to me during downsizing."
+            },
+            {
+                nickname: "The Rainy City",
+                formerLocation: "Closet",
+                currentLocation: "Living Room",
+                description: "A very blue, somewhat abstract painting of a cityscape with a bridge and reflective water in the foreground. It was found in the back of a closet."
+            }
+        ]
     }
-});
+})
