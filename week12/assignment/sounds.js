4d99a49040ffa4a64b6d650e1b4b5e9943df9599
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-05-02 17:23:22 -0500

    Update sounds.js

--- week12/assignment/sounds.js
+++ week12/assignment/sounds.js
@@ -26,9 +26,21 @@ function preload() {
 function setup() {
     canvas = createCanvas(1000, 1000);
     button1 = createButton(wind);
+    button.position(100, 200);
+    button.size(200, 200);
     button2 = createButton(windy);
+    button.position(400, 200);
+    button.size(200, 200);
     button3 = createButton(rain);
+    button.position(100, 200);
+    button.size(200, 200);
     button4 = createButton(rainy);
+    button.position(100, 500);
+    button.size(200, 200);
     button5 = createButton(thunder);
+    button.position(400, 500);
+    button.size(200, 200);
     button6 = createButton(thundery);
+    button.position(700, 500);
+    button.size(200, 200);
 }
