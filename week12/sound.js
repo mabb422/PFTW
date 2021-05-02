6f1dddc76ab0a906045e9c36eb844cf2330800b6
mabb422 <74391422+mabb422@users.noreply.github.com>
2021-05-02 16:31:43 -0500

    Update sound.js

--- week12/sound.js
+++ week12/sound.js
@@ -4,19 +4,27 @@ var circleX = 250;
 var circleY = 250;
 
 function preload() {
+    soundFormats('wav');
     mySound = loadSound('woods.wav');
 }
 
 function setup() {
-    createCanvas(500, 500)
-    mySound.setVolume(0.1);
-    mySound.play();
+    let cnv = createCanvas(500, 500)
+    cnv.mousePressed(canvasPressed);
+    amplitude = new p5.Amplitude();
 }
 
 function draw() {
+    strokeWeight(5);
+    stroke('magenta');
+    noFill();
+    ellipse(250,250,100,100);
     level = mySound.getLevel()
     var diameterX = level + 2500
     var diameterY = level + 2500 * random (1.5)
-
     ellipse(circleX, circleY, diameterX, diameterY)
+}
+
+function canvasPressed() {
+    mySound.play();
 }
